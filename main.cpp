d512982c8ed3b5ccfa1e74c800af84ed7b234500
Mattia222 <127505321+Mattia222@users.noreply.github.com>
2023-05-19 13:48:57 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,12 @@
 #include <iostream>
-
+#include "lib.h"
 int main(){
+  int dim;
+    cin >> dim;
+    float array[dim];
+    for (int i=0;i<dim;i++){
+        cin >> array[i];
+    }
+    cout << funzione (array, dim) << endl;
   return 0;
 }
