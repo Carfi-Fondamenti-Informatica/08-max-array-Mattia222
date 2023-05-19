96bc159deb807491c02d8ce341777879500678db
Mattia222 <127505321+Mattia222@users.noreply.github.com>
2023-05-19 13:49:57 +0200

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,11 @@
+#include "lib.h"
 
+float funzione (float a[], int dim){
+    float max=a[0];
+    for (int i=0;i<dim;i++){
+        if (a[i]>max){
+            max=a[i];
+        }
+    }
+    return max;
+}
